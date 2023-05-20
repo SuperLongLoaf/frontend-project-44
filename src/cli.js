0874f64a3ed9e869430a6767baccab45ee337570
Melnik <melnik@Konstantins-Mac-Pro.local>
2023-05-20 14:23:50 +0100

    Added src/

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+import * as readlineSync from 'readline-sync';
+
+const greeting = () => {
+const userName = readlineSync.question('May I have your name? ');
+console.log('Hello, ' + userName + '!');
+};
+export {greeting};
